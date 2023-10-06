f1c62aa9a841fe827a6e225adc41844b6ec71ace
Yoo Dongryul <dony435@naver.com>
2023-10-06 12:26:14 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier', '@emotion'],
+  rules: {
+    'linebreak-style': ['error', 'unix'],
+    'react-hooks/exhaustive-deps': ['warn'],
+    '@typescript-eslint/no-empty-interface': ['off'],
+  },
+};
